280b90525860a20f76205a3c46b4024f6518ba69
jelenac11 <52001925+jelenac11@users.noreply.github.com>
2021-03-29 03:04:50 +0200

    inicijalni projekti

---
+++ bolnica/src/main/java/com/tim9/bolnica/BolnicaApplication.java
@@ -0,0 +1,13 @@
+package com.tim9.bolnica;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BolnicaApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(BolnicaApplication.class, args);
+	}
+
+}
---
+++ bolnica/src/test/java/com/tim9/bolnica/BolnicaApplicationTests.java
@@ -0,0 +1,13 @@
+package com.tim9.bolnica;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BolnicaApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
