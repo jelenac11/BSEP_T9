465c7b9f7dcafc5c9fe2ee35f0cdde50c922a841
jelenac11 <52001925+jelenac11@users.noreply.github.com>
2021-05-01 00:45:37 +0200

    hospital device

---
+++ bolnicki-uredjaj/src/main/java/com/tim9/bolnickiuredjaj/BolnickiUredjajApplication.java
@@ -0,0 +1,13 @@
+package com.tim9.bolnickiuredjaj;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BolnickiUredjajApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(BolnickiUredjajApplication.class, args);
+	}
+
+}
